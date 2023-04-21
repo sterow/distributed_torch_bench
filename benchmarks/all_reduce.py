3580740afdc050a05611f62517327ec1dc27a311
Stero Wang <sterowang@hotmail.com>
2023-04-21 18:54:25 +0000

    Added warming up stage, refined message printing.

--- benchmarks/all_reduce.py
+++ benchmarks/all_reduce.py
@@ -3,26 +3,44 @@ import time
 
 import torch
 
-backend="gloo" if torch.cuda.nccl.version() is None or torch.cuda.device_count() == 0 else "nccl"
+def all_reduce(x):
+    torch.distributed.all_reduce(x)
+    torch.cuda.synchronize()
+
+if "TORCH_DIST_BACKEND" in os.environ:
+    backend = os.environ["TORCH_DIST_BACKEND"]
+else:
+    backend = "gloo" if torch.cuda.nccl.version() is None or torch.cuda.device_count() == 0 else "nccl"
+print(f"Initializing torch.distributed with backend \"{backend}\"")
 torch.distributed.init_process_group(backend=backend, init_method="env://")
 assert torch.distributed.is_initialized()
-print(f"torch.distributed initialized, backend: {backend}, rank {torch.distributed.get_rank()}, size {torch.distributed.get_world_size()}")
+print(f"torch.distributed initialized, backend: \"{backend}\", rank {torch.distributed.get_rank()}, size {torch.distributed.get_world_size()}", flush = True)
 
 local_rank = int(os.environ["LOCAL_RANK"])
+is_master = torch.distributed.get_rank() == 0
 
 n = 1024 * 1024 * 1024
+outer_loop = 10
+inner_loop = 1
 
 x = torch.randn(n).cuda(local_rank)
 
-if torch.distributed.get_rank() == 0:
+warming_up = True
+
+if is_master:
     for k in os.environ:
         if k.startswith("NCCL"):
-            print(k, "=", os.environ[k])
+            print(k, "=", os.environ[k], flush = True)
+    print("Message size: {:.0f} MB, loop {} * {}".format(n / 1024 / 1024, outer_loop, inner_loop), flush = True)
+    print("Warming up ...", flush = True)
 
-
-for _ in range(10):
+for _ in range(outer_loop + 1):
     t = time.time()
-    torch.distributed.all_reduce(x)
-    torch.cuda.synchronize()
-    if torch.distributed.get_rank() == 0:
-        print("{:.0f} MB: {:.1f} GB/s".format(n / 1024 / 1024, n * 4 / (time.time() - t) / 1024 / 1024 / 1024))
+    for _ in range(inner_loop):
+        all_reduce(x)
+    if is_master:
+        if warming_up:
+            print("Begin testing ...", flush = True)
+            warming_up = False
+        else:
+            print("{:.1f} GB/s".format(n * 4 * inner_loop / (time.time() - t) / 1024 / 1024 / 1024), flush = True)
