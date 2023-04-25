807ec0dbfa2380de9a9f35d24be1d9ad7e6c221c
Bin Wang <biwang@microsoft.com>
2023-04-25 14:27:14 +0800

    Fix warming up reporting

--- benchmarks/all_reduce.py
+++ benchmarks/all_reduce.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 import torch
 
+def all_reduce(x):
+    torch.distributed.all_reduce(x)
+    torch.cuda.synchronize()
+
 def print_nccl_env_vars():
     for k in os.environ:
         if k.startswith("NCCL"):
             print(k, "=", os.environ[k])
-
-def all_reduce(x):
-    torch.distributed.all_reduce(x)
-    torch.cuda.synchronize()
 
 if "TORCH_DISTRIBUTED_BACKEND" in os.environ:
     backend = os.environ["TORCH_DISTRIBUTED_BACKEND"]
@@ -27,7 +27,7 @@ is_master = torch.distributed.get_rank() == 0
 local_rank = int(os.environ["LOCAL_RANK"])
 if local_rank == 0: print_nccl_env_vars()
 
-n = 3 * 1024 * 1024 * 1024
+n = 16 * 1024 * 1024 * 1024
 outer_loop = 1000
 inner_loop = 10
 
@@ -42,11 +42,11 @@ for _ in range(outer_loop + 1):
     for _ in range(inner_loop):
         all_reduce(x)
     t_end = time.time()
-    if warming_up:
-        warming_up = False
-        if is_master: print("Start testing now ...")
     if is_master:
         str_date_time = datetime.fromtimestamp(t_end).strftime("%m-%d %H:%M:%S")
         print("{} - {:.1f} GB/s".format(str_date_time, n * 4 * inner_loop / (t_end - t_begin) / 1024 / 1024 / 1024))
+    if warming_up:
+        warming_up = False
+        if is_master: print("Start testing now ...")
 
 if is_master: print("Testing has ended.")
